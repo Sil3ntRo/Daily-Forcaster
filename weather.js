d6a49304f505dc91c380b08fb77c820d49a3db1e
Rohan Gayle <rgayle33614@gmail.com>
2017-05-10 00:56:19 -0400

    Resolved Styling Issues
    *Resolved image consistency (images could be viewed locally, but failed
    to show up within browers)
    * Resolved calculations and measurements on the Forecast page

--- weather.js
+++ weather.js
@@ -13,7 +13,7 @@ function getWeather(){
 	if(city != ''){
 		
 		$.ajax({
-			url: 'http://api.openweathermap.org/data/2.5/weather?q=' + city + "&units=metric" + "&APPID=dfc20fb3393b7759966b0b6c30c5264a",
+			url: 'http://api.openweathermap.org/data/2.5/weather?q=' + city + "&units=imperial" + "&APPID=dfc20fb3393b7759966b0b6c30c5264a",
 			type: "GET",
 			dataType: 'jsonp',
 			success: function(data){
@@ -35,12 +35,12 @@ function showResults(data){
 	return '<h2 style="font-weight:bold; font-size:30px; padding-top:20px" class="text-center">Current Weather for ' +data.name+', ' +data.sys.country+ '</h2>' +
 	"<h3 style='padding-left:40px;'><strong>Weather</strong>: " +data.weather[0].main+"</h3>" +
 	"<h3 style='padding-left:40px;'><strong>Weather Description</strong>: <img src='http://openweathermap.org/img/w/" +data.weather[0].icon+".png'> "+data.weather[0].description+"</h3>" +
-	"<h3 style='padding-left:40px;'><strong>Temperature</strong>: "+data.main.temp+" &deg;C</h3>" +
+	"<h3 style='padding-left:40px;'><strong>Temperature</strong>: "+data.main.temp+" &deg;F</h3>" +
 	"<h3 style='padding-left:40px;'><strong>Pressure</strong>: " +data.main.pressure+" hpa</h3>" +
     "<h3 style='padding-left:40px;'><strong>Humidity</strong>: " +data.main.humidity+"%</h3>" +
-    "<h3 style='padding-left:40px;'><strong>Min Temperature</strong>: " +data.main.temp_min+"&deg;C</h3>" +
-    "<h3 style='padding-left:40px;'><strong>Max Temperature</strong>: " +data.main.temp_max+"&deg;C</h3>" +
-    "<h3 style='padding-left:40px;'><strong>Wind Speed</strong>: " +data.wind.speed+"m/s</h3>" +
+    "<h3 style='padding-left:40px;'><strong>Min Temperature</strong>: " +data.main.temp_min+"&deg;F</h3>" +
+    "<h3 style='padding-left:40px;'><strong>Max Temperature</strong>: " +data.main.temp_max+"&deg;F</h3>" +
+    "<h3 style='padding-left:40px;'><strong>Wind Speed</strong>: " +data.wind.speed+"mi/hr</h3>" +
 	"<h3 style='padding-left:40px; padding-bottom:30px;'><strong>Wind Direction</strong>: " +data.wind.deg+"&deg;</h3>";
 }
 	
